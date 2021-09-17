e9e75adbc38a23e565273f08296d224d7469a889
MelonHell <MelonHell@ya.ru>
2021-09-17 23:27:41 +0300

    добавил LambdaCommand

---
+++ src/MeloncherWPF/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeloncherWPF.Infrastructure.Commands
+{
+	class LambdaCommand
+	{
+	}
+}
