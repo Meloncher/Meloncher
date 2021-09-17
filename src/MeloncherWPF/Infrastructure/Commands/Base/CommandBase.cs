63009b87f1897d01e749d4553ca723fa108900d9
MelonHell <MelonHell@ya.ru>
2021-09-17 23:17:42 +0300

    добавил CommandBase

---
+++ src/MeloncherWPF/Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MeloncherWPF.Infrastructure.Commands.Base
+{
+	internal abstract class CommandBase : ICommand
+	{
+		public event EventHandler CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+		public abstract bool CanExecute(object parameter);
+		public abstract void Execute(object parameter);
+	}
+}
