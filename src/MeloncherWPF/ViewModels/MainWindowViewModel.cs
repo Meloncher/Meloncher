acc71f7cc874eab0a24601933a4b9f5f042c674b
MelonHell <MelonHell@ya.ru>
2021-09-18 00:23:05 +0300

    шот доделол шот переделол

--- src/MeloncherWPF/ViewModels/MainWindowViewModel.cs
+++ src/MeloncherWPF/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using MeloncherWPF.Infrastructure.Commands;
 using MeloncherWPF.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,71 +13,49 @@ using System.Windows.Input;
 
 namespace MeloncherWPF.ViewModels
 {
-	internal class MainWindowViewModel : ViewModelBase
+	internal class MainWindowViewModel : INotifyPropertyChanged
 	{
-		private string _Title = "Meloncher";
-		public string Title
-		{
-			get => _Title;
-			set => Set(ref _Title, value);
-		}
 
-		private string _Version = "1.12.2";
-		public string Verison
+		public MainWindowViewModel()
 		{
-			get => _Version;
-			set => Set(ref _Version, value);
+			Title = "Meloncher";
+			McVersionName = "1.12.2";
+			Username = "Steve";
+			Optifine = true;
+			Offline = false;
+			ProgressValue = 0;
+			IsNotStarted = true;
+			PlayButtonCommand = new LambdaCommand(OnPlayButtonCommandExecuted, CanPlayButtonCommandExecute);
 		}
-
-		private string _Username = "Steve";
-		public string Username
-		{
-			get => _Username;
-			set => Set(ref _Username, value);
-		}
-
-		private bool _Optifine = true;
-		public bool Optifine
-		{
-			get => _Optifine;
-			set => Set(ref _Optifine, value);
-		}
-
-		private bool _Offline = false;
-		public bool Offline
-		{
-			get => _Offline;
-			set => Set(ref _Offline, value);
-		}
-
-		private int _ProgressValue = 0;
-		public int ProgressValue
-		{
-			get => _ProgressValue;
-			set => Set(ref _ProgressValue, value);
-		}
+		
+		public string Title { get; set; }
+		public string McVersionName { get; set; }
+		public string Username { get; set; }
+		public bool Optifine { get; set; }
+		public bool Offline { get; set; }
+		public int ProgressValue { get; set; }
+		public bool IsNotStarted { get; set; }
 
 		private McLauncher mcLauncher = new McLauncher();
-		private bool isStarted = false;
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		public ICommand PlayButtonCommand { get; }
 		private bool CanPlayButtonCommandExecute(object p)
 		{
-			return !isStarted;
+			//return !IsStarted;
+			return true;
 		}
 		private void OnPlayButtonCommandExecuted(object p)
 		{
 			mcLauncher = new McLauncher();
 			new Task(async () => {
-				isStarted = true;
-				await mcLauncher.Launch(new McVersion(Verison, "Test", "Test-" + Verison), MSession.GetOfflineSession(Username), Offline, Optifine);
-				isStarted = false;
+				IsNotStarted = false;
+				Title = "Meloncher " + McVersionName;
+				await mcLauncher.Launch(new McVersion(McVersionName, "Test", "Test-" + McVersionName), MSession.GetOfflineSession(Username), Offline, Optifine);
+				IsNotStarted = true;
+				Title = "Meloncher";
 			}).Start();
-		}
-
-		public MainWindowViewModel()
-		{
-			PlayButtonCommand = new LambdaCommand(OnPlayButtonCommandExecuted, CanPlayButtonCommandExecute);
 		}
 	}
 }
