6e2a9c1ee9ede27582a01e5b33a4ddafa9e4f88c
MelonHell <MelonHell@ya.ru>
2021-11-03 20:11:55 +0300

    Beta 1.1.0

--- src/MeloncherAvalonia/Models/Updater.cs
+++ src/MeloncherAvalonia/Models/Updater.cs
@@ -9,7 +9,7 @@ namespace MeloncherAvalonia.Models
 {
 	public class Updater
 	{
-		private const int CurrentVersion = 18;
+		private const int CurrentVersion = 19;
 		private readonly WebClient _client = new();
 		private UpdaterJson? _updaterJson;
 
